8e833cb26ae12b53894017af5e8b29678acc8225
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2020-02-19 17:08:51 -0400

    Add project files.

---
+++ Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParcialAplicada2.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source = Database\ExameDb.db");
+        }
+    }
+}
