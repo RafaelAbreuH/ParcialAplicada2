e8723e289151177587877bfcd72875f095edd2d5
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2020-02-19 18:21:33 -0400

    commit 1

---
+++ Models/Inscripcions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParcialAplicada2.Models
+{
+    public class Inscripcions
+    {
+        [Key]
+        public int InscripcionId { get; set; }
+        public string Semestre { get; set; }
+        public int Limite { get; set; }
+        public int Tomados { get; set; }
+        public int Disponible { get; set; }
+
+        public Inscripcions()
+        {
+            InscripcionID = 0;
+            Semestre = String.Empty;
+            Limite = 0;
+            Tomados = 0;
+            Disponible = 0;
+        }
+    }
+}
---
+++ controller/InscripcionController.cs
@@ -0,0 +1,28 @@
+﻿using ParcialAplicada2.Data;
+using ParcialAplicada2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParcialAplicada2.controller
+{
+    public class InscripcionController
+    {
+        public bool Guardar(Inscripcions inscripcion)
+        {
+            Contexto db = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                if(inscripcion.InscripcionId == 0)
+                {
+                    paso =
+                }
+            }
+
+        }
+        
+    }
+}
